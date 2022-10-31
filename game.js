edc3e46d40cb6e3c386dce3757357d1b94a9deec
xxchillkroetexx <info@wolf-andreas.eu>
2022-10-31 14:47:43 +0100

    basic logic in game.js with comments

--- game.js
+++ game.js
@@ -1,1 +1,26 @@
 //SPIELLOGIK
+
+// Spielkarten aus ./assets/spielkarten.json in Array "cards" importieren
+async function getCards() {
+    const requestURL = "https://github.com/xxchillkroetexx/Projektmanagement/blob/main/assets/spielkarten.json";
+    const request = new Request(requestURL);
+
+    const response = await fetch(request);
+    const cards = await response.json();
+    return cards;
+}
+
+
+
+function showCard() {
+    document.getElementById("foot").innerText = getCards();
+}
+showCard();
+
+// Zufällige Karte aus Array "cards" anzeigen
+
+// Buttons mit Funktion versehen
+
+// Anzeige unterbinden, wenn Timer auf 00:00 steht
+// Teams wechseln
+// Neue Karte anzeigen
