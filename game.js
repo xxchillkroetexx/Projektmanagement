11a0fbc73d55c278d18dc21ba2b82fa19222bd22
xxchillkroetexx <info@wolf-andreas.eu>
2022-11-04 14:41:12 +0100

    timerscript in game.js integriert

--- game.js
+++ game.js
@@ -1,26 +1,75 @@
 //SPIELLOGIK
 
 // Spielkarten aus ./assets/spielkarten.json in Array "cards" importieren
-async function getCards() {
-    const requestURL = "https://github.com/xxchillkroetexx/Projektmanagement/blob/main/assets/spielkarten.json";
-    const request = new Request(requestURL);
+fetch("./assets/spielkarten.json")
+    .then(response => {
+        return response.json
+    })
+    .then(jsondata => console.info(jsondata));
+console.log("hello World");
 
-    const response = await fetch(request);
-    const cards = await response.json();
-    return cards;
+/**
+ *  Zufällige Karte aus Array "cards" anzeigen
+ */
+
+/** 
+ * Buttons mit Funktion versehen
+ */
+
+/**
+ * TIMER LAUFEN LASSEN
+ */
+
+// Set the date we're counting down to
+localStorage.time = 1000; // spaeter: abfrage von eingabe
+
+// Update the count down every 1 second
+
+function countdown() {
+    time = parseInt(localStorage.time);
+
+    if (isNaN(time) || time > (30 * 60)) {
+        alert("An error occured: time left variable is corrupted, resetting timer");
+        localStorage.time = 30 * 60; //30 minutes in seconds
+        countdown();
+        return null;
+    }
+
+    /* TIMER ABGELAUFEN */
+    if (time <= 0) {
+        alert("TIMER EXPIRED!");
+        return null;
+    }
+
+    //Decrementing time and recalling the function in 1 second
+    document.getElementById("timer").innerText = formatTime(time);
+    time--;
+    localStorage.time = time;
+    setTimeout("countdown()", 1000);
+
 }
 
+function formatTime(time) {
+    minutes = Math.floor(time / 60);
+    seconds = time - minutes * 60;
 
-
-function showCard() {
-    document.getElementById("foot").innerText = getCards();
+    if (String(seconds).length == 1) {
+        return String(minutes) + ":0" + String(seconds);
+    }
+    return String(minutes) + ":" + String(seconds);
 }
-showCard();
 
-// Zufällige Karte aus Array "cards" anzeigen
+countdown();
 
-// Buttons mit Funktion versehen
 
-// Anzeige unterbinden, wenn Timer auf 00:00 steht
-// Teams wechseln
-// Neue Karte anzeigen
+/**
+ * Anzeige unterbinden, wenn Timer auf 00:00 steht
+ */
+
+/**
+ * Teams wechseln 
+ */
+
+/**
+ * Neue Karte anzeigen
+ */
